e9f60cb52406aac7ea8938b3441445cc0ad97ac6
gougouccnu <lishaoweiccnu@163.com>
2017-04-07 23:24:19 +0800

    add zoomIt

--- firebaseapp/public/static/app/controller.js
+++ firebaseapp/public/static/app/controller.js
@@ -781,7 +781,39 @@ $scope.check_movement = function(){
 };
 
 
+$scope.zoomIt = function(factor) {
+canvas.setHeight(canvas.getHeight() * factor);
+canvas.setWidth(canvas.getWidth() * factor);
+if (canvas.backgroundImage) {
+    // Need to scale background images as well
+    var bi = canvas.backgroundImage;
+    bi.width = bi.width * factor; bi.height = bi.height * factor;
+}
+var objects = canvas.getObjects();
+for (var i in objects) {
+    var scaleX = objects[i].scaleX;
+    var scaleY = objects[i].scaleY;
+    var left = objects[i].left;
+    var top = objects[i].top;
+
+    var tempScaleX = scaleX * factor;
+    var tempScaleY = scaleY * factor;
+    var tempLeft = left * factor;
+    var tempTop = top * factor;
+
+    objects[i].scaleX = tempScaleX;
+    objects[i].scaleY = tempScaleY;
+    objects[i].left = tempLeft;
+    objects[i].top = tempTop;
+
+    objects[i].setCoords();
+}
+canvas.renderAll();
+canvas.calcOffset();
+}
+
 $scope.segment = function () {
+    $scope.zoomIt(1.5);
     $scope.setFreeDrawingMode(false,$scope.current_mode);
     $scope.check_movement();
     if (state.masks_present) {
@@ -894,8 +926,6 @@ function watchCanvas($scope) {
     .on('selection:cleared', updateScope)
     .on('path:created', segmentBtnClicked);
 }
-
-
 
 cveditor.controller('CanvasControls', function($scope) {
   $scope.convnet_mode = false;
